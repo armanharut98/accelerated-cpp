221ce2af8022337511afbb6f6f4bddc0b93257ec
Arman Harutyunyan <armanharutyunyan451@gmail.com>
2022-11-05 18:06:04 +0400

    Exercises from Part 3

--- part-2/exercises/ex2.10.cpp
+++ part-2/exercises/ex2.10.cpp
@@ -4,7 +4,7 @@ int main() {
     int k = 0;
     while (k != 0) {
         using std::cout;
-        cout << "*";
+        cout << "*"; // std:: is not required here because of the using statement above
         ++k;
     }
     std::cout << std::endl; // std:: is required here
