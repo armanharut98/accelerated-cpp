7a3593bfb907dde39ab4ba875c79317d06d86251
Arman Harutyunyan <armanharutyunyan451@gmail.com>
2023-01-15 09:47:49 +0400

    Part 4 dir removed

--- part-4/exercises/count_words.h
+++
@@ -1,9 +0,0 @@
-#ifndef GUARD_count_duplicates
-#define GUARD_count_duplicates
-
-#include <vector>
-#include <string>
-
-std::vector<int> count_words(std::vector<std::string> words, std::vector<std::string>& words_unique);
-
-#endif
--- part-4/exercises/read_strings.h
+++
@@ -1,9 +0,0 @@
-#ifndef GUARD_read_strings
-#define GUARD_read_strings
-
-#include <vector>
-#include <string>
-
-void read_strings(std::vector<std::string>& words);
-
-#endif
